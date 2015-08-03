1ecf669b4a0aab50805bcff8e7dc0bff2493fbc2
Adrian <adrian07rodriguez@gmail.com>
2015-08-02 22:09:16 -0700

    Adding project files
    Signed-off-by: Adrian <adrian07rodriguez@gmail.com>

---
+++ Trichromatic/Areas/Admin/Controllers/FileManagerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trichromatic.Areas.Admin.Controllers
+{
+    public class FileManagerController : Controller
+    {
+        //
+        // GET: /Admin/FileManager/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
