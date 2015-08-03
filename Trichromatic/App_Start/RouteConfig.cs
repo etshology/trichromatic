9bcc58755765f7cf41eadfc3d6e4d10c8fd59dc1
Adrian <adrian07rodriguez@gmail.com>
2015-08-02 22:11:46 -0700

    Added file manager code
    Signed-off-by: Adrian <adrian07rodriguez@gmail.com>

--- Trichromatic/App_Start/RouteConfig.cs
+++ Trichromatic/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace Trichromatic
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("elfinder.connector");
 
             routes.MapRoute(
                 name: "Default",
--- Trichromatic/Global.asax.cs
+++ Trichromatic/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,16 +7,46 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+using Autofac;
+using elFinder.Connector.Integration.Autofac;
+
 namespace Trichromatic
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static IContainer _container;
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("elfinder.connector");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            // register IoC
+            var builder = new ContainerBuilder();
+            builder.RegisterElFinderConnectorDefault();
+            _container = builder.Build();
+            // need also to set container in elFinder module
+            _container.SetAsElFinderDependencyResolver();
         }
     }
 }
